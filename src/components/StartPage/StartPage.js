bd72077c648989a4dd2fe97acfadfcefa7d3c407
Ilya Sidorchik <ilya@sidorchik.ru>
2019-09-24 21:00:00 +0300

    Make own structure

---
+++ src/components/StartPage/StartPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StartPage() {
+	return (
+		<div className="StartPage">
+			StartPage
+		</div>
+	);
+}
+
+export default StartPage;
--- src/components/shared/App/App.js
+++ src/components/shared/App/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import './App.css';
+import StartPage from '../../StartPage';
 
 function App() {
 	return (
 		<div className="App">
-			Page
+			<StartPage />
 		</div>
 	);
 }
