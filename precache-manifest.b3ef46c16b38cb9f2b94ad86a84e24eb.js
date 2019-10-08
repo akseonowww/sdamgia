a5e99e18fde0f8f9b1715d91aaa7ad4e89315eef
Ilya Sidorchik <ilya@sidorchik.ru>
2019-10-08 18:23:19 +0300

    Updates

---
+++ precache-manifest.b3ef46c16b38cb9f2b94ad86a84e24eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7dbe472a0598d339845d3770ce200cf",
+    "url": "/sdamgia-variant-constructor/index.html"
+  },
+  {
+    "revision": "5501523d83cc9eae65da",
+    "url": "/sdamgia-variant-constructor/static/css/main.52b7b944.chunk.css"
+  },
+  {
+    "revision": "188ee7ecd4143d48a129",
+    "url": "/sdamgia-variant-constructor/static/js/2.caecbf53.chunk.js"
+  },
+  {
+    "revision": "5501523d83cc9eae65da",
+    "url": "/sdamgia-variant-constructor/static/js/main.14ea4873.chunk.js"
+  },
+  {
+    "revision": "e5a85f1a37d29281d51f",
+    "url": "/sdamgia-variant-constructor/static/js/runtime-main.17e9285f.js"
+  },
+  {
+    "revision": "488c457415ef6c76a453799af47555cb",
+    "url": "/sdamgia-variant-constructor/static/media/contentBannerBottomAd.488c4574.png"
+  },
+  {
+    "revision": "06163253e826686b31e63f197553db20",
+    "url": "/sdamgia-variant-constructor/static/media/contentBannerTopAd.06163253.png"
+  },
+  {
+    "revision": "931231aebf8ad3daef03c6c4cb511cf6",
+    "url": "/sdamgia-variant-constructor/static/media/sidebarBannerAd.931231ae.png"
+  },
+  {
+    "revision": "de0bd41b156ccdf0e3a4382947b97c6a",
+    "url": "/sdamgia-variant-constructor/static/media/tick.de0bd41b.svg"
+  },
+  {
+    "revision": "8599a7e0ce40aed29232e1c53b87a353",
+    "url": "/sdamgia-variant-constructor/static/media/tick_blue.8599a7e0.svg"
+  }
+]);
