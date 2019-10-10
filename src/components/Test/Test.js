c071062bceb092ea7aa30f13d5b14e268cf775d5
Ilya Sidorchik <ilya@sidorchik.ru>
2019-10-11 02:05:29 +0300

    Add cork text in Test

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import Section from '../shared/Section';
+import Title from '../shared/Title';
 import './Test.css';
 
 const Test = () => (
-    <div>Тест</div>
+	<Section class="Test">
+		<Title>Тест</Title>
+		<p class="Section-Desc">
+			При выполнении заданий с кратким ответом впишите в поле для ответа
+			цифру, которая соответствует номеру правильного ответа, или число,
+			слово, последовательность букв (слов) или цифр. Ответ следует
+			записывать без пробелов и каких-либо дополнительных символов.
+			Дробную часть отделяйте от целой десятичной запятой. Единицы
+			измерений писать не нужно.
+		</p>
+	</Section>
 );
 
 export default Test;
