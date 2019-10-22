a7d78191265c75b8bea192b7c38032aa341b2dca
Ilya Sidorchik <ilya@sidorchik.ru>
2019-10-22 11:39:35 +0300

    Adopt placeholder in CatalogSearch

--- src/components/Student/CatalogSearch/CatalogSearch.js
+++ src/components/Student/CatalogSearch/CatalogSearch.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 
 import Title from '../../shared/Title';
 import '../../shared/Form/Form.css';
@@ -6,27 +6,49 @@ import '../../shared/Input/Input.css';
 import '../../shared/Button/Button.css';
 import './CatalogSearch.css';
 
-const CatalogSearch = () => (
-	<div className="CatalogSearch">
-		<Title>Поиск в каталоге</Title>
+const useWindowWidth = () => {
+	const [width, setWidth] = useState(0);
 
-		<p className="Section-Desc">
-			Все задания открытого банка ЕГЭ по математике профильного уровня
-			с образцами&nbsp;решений.
-		</p>
+	useLayoutEffect(() => {
+		const updateWidth = () => setWidth(window.innerWidth);
 
-		<form action="/test" method="POST">
-			<div className="Form_oneLine">
-				<input
-					className="Input Form_oneLine-Input"
-					type="text"
-					name="id"
-					placeholder="№/текст задания"
-				/>
-				<input className="Button" type="submit" value="Открыть" />
-			</div>
-		</form>
-	</div>
-);
+		window.addEventListener('resize', updateWidth);
+		updateWidth();
+
+		return () => window.removeEventListener('resize', updateWidth);
+	}, []);
+
+	return width;
+};
+
+const CatalogSearch = () => {
+	const windowWidth = useWindowWidth();
+
+	const placeholder =
+		windowWidth > 836 ? 'Номер или текст задания' : '№/текст задания';
+
+	return (
+		<div className="CatalogSearch">
+			<Title>Поиск в каталоге</Title>
+
+			<p className="Section-Desc">
+				Все задания открытого банка ЕГЭ по математике профильного уровня
+				с образцами&nbsp;решений.
+			</p>
+
+			<form action="/test" method="POST">
+				<div className="Form_oneLine">
+					<input
+						className="Input Form_oneLine-Input"
+						type="text"
+						name="id"
+						placeholder={placeholder}
+					/>
+					<input className="Button" type="submit" value="Открыть" />
+				</div>
+			</form>
+		</div>
+	);
+};
 
 export default CatalogSearch;
