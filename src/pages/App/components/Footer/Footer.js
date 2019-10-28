d65522472d2c6d94309f307028c85de91835c0bc
Ilya Sidorchik <ilya@sidorchik.ru>
2019-10-28 15:05:03 +0300

    Restyle footer with flexbox

--- src/pages/App/components/Footer/Footer.js
+++ src/pages/App/components/Footer/Footer.js
@@ -6,30 +6,27 @@ import spbsut from './assets/spbsut.png';
 
 const Footer = ({ className }) => (
 	<footer className={`Footer ${className}`}>
-		<p style={{ float: 'left' }}>
+		<p>
 			<a class="Link Link_black" href="/about">
 				О проекте
 			</a>{' '}
-			· <a class="Link Link_black" href="/staff">Редакция</a> ·{' '}
+			·{' '}
+			<a class="Link Link_black" href="/staff">
+				Редакция
+			</a>{' '}
+			·{' '}
 			<a class="Link Link_black" href="/legal">
 				Правовая информация
 			</a>{' '}
 		</p>
-		<p align="right">©&nbsp;Гущин Д. Д., 2011—2019</p> <br />
-		<center style={{ clear: 'both' }}>
-			<a
-				href="http://priem.sut.ru/"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<img
-					src={spbsut}
-					alt="СПб ГУТ!"
-					title="priem.sut.ru"
-					width="160"
-				/>
-			</a>
-		</center>
+		<a
+			href="http://priem.sut.ru/"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<img src={spbsut} alt="СПб ГУТ!" title="priem.sut.ru" width="160" />
+		</a>
+		<p>©&nbsp;Гущин Д. Д., 2011—2019</p>
 	</footer>
 );
 
