ad21851e568c9a5b64d6f605217ab752521cbf85
Ilya Sidorchik <ilya@sidorchik.ru>
2019-10-30 00:04:22 +0300

    Refactor Header

--- src/pages/App/components/Header/Header.js
+++ src/pages/App/components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import cx from 'classnames';
 
 import '../../../../components/Link/Link.scss';
 import '../../../../components/Link/_selected/Link_selected.scss';
 import './Header.scss';
 
 const Header = ({ className }) => (
-	<header className={`Header ${className}`}>
+	<header className={cx('Header', className)}>
 		<div className="Header-Main">
-			<NavLink to="/" exact activeClassName="Link_selected">
+			<NavLink activeClassName="Link_selected" to="/" exact>
 				<img
-					src="https://ege.sdamgia.ru/img/headers/logo.svg"
 					className="Header-Logo"
+					src="https://ege.sdamgia.ru/img/headers/logo.svg"
 					alt="Сдам ГИА"
 				/>
 			</NavLink>
