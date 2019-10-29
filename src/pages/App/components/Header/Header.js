f4eafbbd8c9792ac03a2b553fd1ce2f653ceabaa
Ilya Sidorchik <ilya@sidorchik.ru>
2019-10-30 00:00:29 +0300

    Relayout Header from float to flexbox

--- src/pages/App/components/Header/Header.js
+++ src/pages/App/components/Header/Header.js
@@ -7,46 +7,46 @@ import './Header.scss';
 
 const Header = ({ className }) => (
 	<header className={`Header ${className}`}>
-		<NavLink to="/" exact activeClassName="Link_selected">
-			<img
-				src="https://ege.sdamgia.ru/img/headers/logo.svg"
-				className="Header-Logo"
-				alt="Сдам ГИА"
-			/>
-		</NavLink>
+		<div className="Header-Main">
+			<NavLink to="/" exact activeClassName="Link_selected">
+				<img
+					src="https://ege.sdamgia.ru/img/headers/logo.svg"
+					className="Header-Logo"
+					alt="Сдам ГИА"
+				/>
+			</NavLink>
+
+			<div className="Header-Title">
+				<a
+					className="Header-Link Link Link_black"
+					href="https://sdamgia.ru"
+					rel="noopener noreferrer"
+				>
+					СДАМ ГИА
+				</a>
+				:{' '}
+				<NavLink
+					className="Header-Link Link Link_black"
+					activeClassName="Link_selected"
+					to="/"
+					exact
+				>
+					РЕШУ ЕГЭ
+				</NavLink>
+			</div>
+			<div className="Header-WebsiteDesc">
+				Образовательный портал для подготовки к экзаменам
+			</div>
+			<div className="Header-SubjectTitle">
+				Математика профильного уровня
+			</div>
+		</div>
+
 		<img
 			className="Header-SubjectImg"
 			src="https://math.reshuct.by/img/headers/math.png"
-			style={{
-				float: 'right',
-				height: '100px',
-				margin: '5px'
-			}}
 			alt="Математика"
 		/>
-		<div className="Header-Title">
-			<a
-				className="Header-Link Link Link_black"
-				href="https://sdamgia.ru"
-				rel="noopener noreferrer"
-			>
-				СДАМ ГИА
-			</a>
-			:{' '}
-			<NavLink
-				className="Header-Link Link Link_black"
-				activeClassName="Link_selected"
-				to="/"
-				exact
-			>
-				РЕШУ ЕГЭ
-			</NavLink>
-		</div>
-		<div className="Header-WebsiteDesc">
-			Образовательный портал для подготовки к экзаменам
-		</div>
-		<div className="Header-SubjectTitle">Математика профильного уровня</div>
-		<div style={{ clear: 'both' }}></div>
 	</header>
 );
 
