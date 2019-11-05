019c188fecacacb50fc187e123b88fcbabb56b52
Ilya Sidorchik <ilya@sidorchik.ru>
2019-11-05 23:56:26 +0300

    Add validation for empty in ProfileAuth

--- src/pages/App/components/Sidebar/components/ProfileWidget/components/ProfileAuth/ProfileAuth.js
+++ src/pages/App/components/Sidebar/components/ProfileWidget/components/ProfileAuth/ProfileAuth.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import '../../../../../../../../components/Link/Link.scss';
 import '../../../../../../../../components/Button/Button.scss';
@@ -7,6 +7,9 @@ import './ProfileAuth.scss';
 const ProfileAuth = ({ setAuth }) => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+
+	const emailInput = useRef(null);
+	const passwordInput = useRef(null);
 
 	const handleInputChange = e => {
 		const { name, value: newValue } = e.target;
@@ -23,8 +26,22 @@ const ProfileAuth = ({ setAuth }) => {
 		}
 	};
 
+	const handleEmailInputFocus = () => {
+		emailInput.current.focus();
+	};
+
+	const handlePasswordInputFocus = () => {
+		passwordInput.current.focus();
+	};
+
 	const handleSubmit = e => {
 		e.preventDefault();
+
+		if (email === '') {
+			handleEmailInputFocus();
+		} else if (password === '') {
+			handlePasswordInputFocus();
+		}
 
 		if (email && password) setAuth(true);
 	};
@@ -46,6 +63,7 @@ const ProfileAuth = ({ setAuth }) => {
 						name="user"
 						value={email}
 						placeholder="Электронная почта"
+						ref={emailInput}
 						onChange={handleInputChange}
 					/>
 					<input
@@ -54,6 +72,7 @@ const ProfileAuth = ({ setAuth }) => {
 						name="password"
 						value={password}
 						placeholder="Пароль"
+						ref={passwordInput}
 						onChange={handleInputChange}
 					/>
 					<input type="hidden" name="la" value="login" />
