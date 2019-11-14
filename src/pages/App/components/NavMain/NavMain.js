495fc5aa9aa81e4fffdc0fe5fd7dc5fe83847632
Ilya Sidorchik <ilya@sidorchik.ru>
2019-11-14 20:44:37 +0300

    Remove hardcoding in NavMain

--- src/pages/App/components/NavMain/NavMain.js
+++ src/pages/App/components/NavMain/NavMain.js
@@ -2,116 +2,98 @@ import React from 'react';
 
 import './NavMain.scss';
 
-const NavMain = ({ className }) => (
-	<>
-		<nav
-			className={`NavMain NavMain_desktop ${className} ${className}_desktop`}
-		>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://math-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">≡ Математика</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://inf-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Информатика</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://rus-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Русский язык</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://en-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Английский язык</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://de-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Немецкий язык</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://fr-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Французcкий язык</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://sp-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Испанский язык</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://phys-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Физика</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://chem-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Химия</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://bio-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Биология</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://geo-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">География</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://soc-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Обществознание</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://lit-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">Литература</div>
-			</a>
-			<a
-				className="NavMain-Link NavMain_desktop-Link"
-				href="https://hist-ege.sdamgia.ru/?redir=1"
-				rel="nofollow"
-			>
-				<div className="NavMain-Tab NavMain_desktop-Tab">История</div>
-			</a>
-			<div style={{ clear: 'both' }}></div>
-		</nav>
+const NavMain = ({ className }) => {
+	const exam = 'ege';
+	const subjectsList = [
+		{
+			title: 'Информатика',
+			url: 'math'
+		},
+		{
+			title: 'Русский язык',
+			url: 'rus'
+		},
+		{
+			title: 'Английский язык',
+			url: 'en'
+		},
+		{
+			title: 'Немецкий язык',
+			url: 'de'
+		},
+		{
+			title: 'Французcкий язык',
+			url: 'fr'
+		},
+		{
+			title: 'Испанский язык',
+			url: 'sp'
+		},
+		{
+			title: 'Физика',
+			url: 'phys'
+		},
+		{
+			title: 'Химия',
+			url: 'chem'
+		},
+		{
+			title: 'Биология',
+			url: 'bio'
+		},
+		{
+			title: 'География',
+			url: 'geo'
+		},
+		{
+			title: 'Обществознание',
+			url: 'soc'
+		},
+		{
+			title: 'Литература',
+			url: 'lit'
+		},
+		{
+			title: 'История',
+			url: 'hist'
+		}
+	];
 
-		<nav className={`NavMain ${className} ${className}_mobile`}>
-			≡ математика
-		</nav>
-	</>
-);
+	return (
+		<>
+			<nav
+				className={`NavMain NavMain_desktop ${className} ${className}_desktop`}
+			>
+				<a
+					className="NavMain-Link NavMain_desktop-Link"
+					href=""
+					rel="nofollow"
+				>
+					<div className="NavMain-Tab NavMain_desktop-Tab">
+						≡ Математика
+					</div>
+				</a>
+
+				{subjectsList.map(({ title, url }) => (
+					<a
+						className="NavMain-Link NavMain_desktop-Link"
+						href={`https://${url}-${exam}.sdamgia.ru`}
+						rel="nofollow"
+					>
+						<div className="NavMain-Tab NavMain_desktop-Tab">
+							{title}
+						</div>
+					</a>
+				))}
+
+				<div style={{ clear: 'both' }}></div>
+			</nav>
+
+			<nav className={`NavMain ${className} ${className}_mobile`}>
+				≡ математика
+			</nav>
+		</>
+	);
+};
 
 export default NavMain;
