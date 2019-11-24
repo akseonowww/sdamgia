15eb90ddbda7f1bd5e317b56c7c293d25cb75f19
Ilya Sidorchik <ilya@sidorchik.ru>
2019-11-24 16:06:24 +0300

    Prepare NavSide for data fetch

--- src/pages/App/components/Sidebar/components/NavSide/NavSide.js
+++ src/pages/App/components/Sidebar/components/NavSide/NavSide.js
@@ -1,108 +1,134 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 import './NavSide.scss';
 
 const NavSide = () => {
+	const [pages, setPages] = useState(null);
+
+	useEffect(() => {
+		axios
+			.get('http://sidorchik.ru/reshuege/api/sidebar/menu/')
+			.then(response => response.data)
+			.then(data =>
+				data.map(({ url, exact, text, hint }) => ({
+					url,
+					exact,
+					text,
+					hint
+				}))
+			)
+			.then(data => setPages(data))
+			.catch(error => {
+				console.log(error);
+
+				const data = [
+					{
+						url: '/manual',
+						exact: false,
+						text: 'Об экзаменеs',
+						hint: 'Шкалы, статистика, оценивание заданий'
+					},
+					{
+						url: '/prob_catalog',
+						exact: false,
+						text: 'Каталог заданий',
+						hint:
+							'Рубрицированные задания, статистика по решенным и нерешенным заданиям'
+					},
+					{
+						url: '/',
+						exact: true,
+						text: 'Ученику',
+						hint: 'Пройти тестирование, проверить себя'
+					},
+					{
+						url: '/teacher',
+						exact: false,
+						text: 'Учителю',
+						hint:
+							'Создание и проверка работ для учащихся, классный журнал'
+					},
+					{
+						url: '/methodist',
+						exact: false,
+						text: 'Варианты',
+						hint:
+							'Подборка тренировочных работ, реальных экзаменационных вариантов предыдущих лет'
+					},
+					{
+						url: '/expert',
+						exact: false,
+						text: 'Эксперту',
+						hint: 'Критерии проверки работ, за что снижают баллы'
+					},
+					{
+						url: '/course',
+						exact: false,
+						text: 'Школа',
+						hint:
+							'Создать свой курс, задать задание учащимся дистанционно'
+					},
+					{
+						url: '/handbook',
+						exact: false,
+						text: 'Справочник',
+						hint: 'Краткий справочник по предмету'
+					},
+					{
+						url: '/page/theory',
+						exact: false,
+						text: 'Теория',
+						hint: 'Теория по предмету'
+					},
+					{
+						url: '/guestbook',
+						exact: false,
+						text: 'Сказать спасибо',
+						hint: 'Сказать добрые слова редакции'
+					},
+					{
+						url: '/callback?f=faq',
+						exact: false,
+						text: 'Вопрос — ответ',
+						hint: 'Задать вопрос об экзаменах и о работе портала'
+					},
+					{
+						url: '/user_stat',
+						exact: false,
+						text: 'Моя статистика',
+						hint: 'Узнать, какие задания подтянуть'
+					},
+					{
+						url: '/favorite',
+						exact: false,
+						text: 'Избранное',
+						hint: 'Посмотреть отобранные вами задания'
+					}
+				];
+
+				setPages(data);
+			});
+	}, []);
+
 	return (
 		<div className="NavSide">
-			
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/manual"
-				title="Шкалы, статистика, оценивание заданий"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">Об экзамене</u>
-			</a>
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/prob_catalog"
-				title="Рубрицированные задания, статистика по решенным и нерешенным заданиям"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">
-					Каталог заданий
-				</u>
-			</a>
-			<NavLink
-				className="Link Link_black Link_wrap NavSide-Link"
-				activeClassName="NavLink_selected Link_selected"
-				to="/"
-				exact
-				title="Пройти тестирование, проверить себя"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">Ученику</u>
-			</NavLink>
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/teacher"
-				title="Создание и проверка работ для учащихся, классный журнал"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">Учителю</u>
-			</a>
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/methodist"
-				title="Подборка тренировочных работ, реальных экзаменационных вариантов предыдущих лет"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">Варианты</u>
-			</a>
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/expert"
-				title="Критерии проверки работ, за что снижают баллы"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">Эксперту</u>
-			</a>
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/course"
-				title="Создать свой курс, задать задание учащимся дистанционно"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">Школа</u>
-			</a>
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/handbook"
-				title="Краткий справочник по предмету"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">Справочник</u>
-			</a>
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/page/theory"
-				title="Теория по предмету"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">Теория</u>
-			</a>
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/guestbook"
-				title="Сказать добрые слова редакции"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">
-					Сказать спасибо
-				</u>
-			</a>
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/callback?f=faq"
-				title="Задать вопрос об экзаменах и о работе портала"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">
-					Вопрос — ответ
-				</u>
-			</a>
-			<a
-				className="Link Link_black Link_wrap NavSide-Link"
-				href="/user_stat"
-			>
-				<u className="Link-U Link_black-U Link_wrap-U">
-					Моя статистика
-				</u>
-			</a>
-			<a className="Link Link_black Link_wrap NavSide-Link" href="/favorite">
-				<u className="Link-U Link_black-U Link_wrap-U">Избранное</u>
-			</a>
+			{pages &&
+				pages.map(({ url, exact, text, hint }, i) => (
+					<NavLink
+						className="Link Link_black Link_wrap NavSide-Link"
+						activeClassName="NavLink_selected Link_selected"
+						to={url}
+						exact={exact}
+						title={hint}
+						key={i}
+					>
+						<u className="Link-U Link_black-U Link_wrap-U">
+							{text}
+						</u>
+					</NavLink>
+				))}
 		</div>
 	);
 };
