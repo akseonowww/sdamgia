7c997e2db41dfaa59044297ac5209a0e070dd3b8
Ilya Sidorchik <ilya@sidorchik.ru>
2020-02-07 09:40:35 +0300

    Separate data from NavMain

--- src/pages/App/components/NavMain/NavMain.tsx
+++ src/pages/App/components/NavMain/NavMain.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState, useEffect, useCallback } from 'react'
 import cx from 'classnames'
 
+import getSubjectList from 'utils/subjectList'
 import './NavMain.scss'
 
 interface INavMainProps {
@@ -22,75 +23,7 @@ const NavMain: FC<INavMainProps> = ({ className }) => {
 	}, [mobileMenu])
 
 	useEffect(() => {
-		setSubjectList([
-			{
-				title: 'Математика',
-				sublist: [
-					{
-						title: 'Базовый уровень',
-						mobileTitle: 'база',
-						url: 'mathb',
-					},
-					{
-						title: 'Профильный уровень',
-						mobileTitle: 'профиль',
-						url: 'math',
-					},
-				],
-			},
-			{
-				title: 'Информатика',
-				url: 'inf',
-			},
-			{
-				title: 'Русский язык',
-				url: 'rus',
-			},
-			{
-				title: 'Английский язык',
-				url: 'en',
-			},
-			{
-				title: 'Немецкий язык',
-				url: 'de',
-			},
-			{
-				title: 'Французcкий язык',
-				url: 'fr',
-			},
-			{
-				title: 'Испанский язык',
-				url: 'sp',
-			},
-			{
-				title: 'Физика',
-				url: 'phys',
-			},
-			{
-				title: 'Химия',
-				url: 'chem',
-			},
-			{
-				title: 'Биология',
-				url: 'bio',
-			},
-			{
-				title: 'География',
-				url: 'geo',
-			},
-			{
-				title: 'Обществознание',
-				url: 'soc',
-			},
-			{
-				title: 'Литература',
-				url: 'lit',
-			},
-			{
-				title: 'История',
-				url: 'hist',
-			},
-		])
+		setSubjectList(getSubjectList())
 	}, [])
 
 	return (
---
+++ src/utils/subjectList.ts
@@ -0,0 +1,71 @@
+const getSubjectList = () => [
+	{
+		title: 'Математика',
+		sublist: [
+			{
+				title: 'Базовый уровень',
+				mobileTitle: 'база',
+				url: 'mathb',
+			},
+			{
+				title: 'Профильный уровень',
+				mobileTitle: 'профиль',
+				url: 'math',
+			},
+		],
+	},
+	{
+		title: 'Информатика',
+		url: 'inf',
+	},
+	{
+		title: 'Русский язык',
+		url: 'rus',
+	},
+	{
+		title: 'Английский язык',
+		url: 'en',
+	},
+	{
+		title: 'Немецкий язык',
+		url: 'de',
+	},
+	{
+		title: 'Французcкий язык',
+		url: 'fr',
+	},
+	{
+		title: 'Испанский язык',
+		url: 'sp',
+	},
+	{
+		title: 'Физика',
+		url: 'phys',
+	},
+	{
+		title: 'Химия',
+		url: 'chem',
+	},
+	{
+		title: 'Биология',
+		url: 'bio',
+	},
+	{
+		title: 'География',
+		url: 'geo',
+	},
+	{
+		title: 'Обществознание',
+		url: 'soc',
+	},
+	{
+		title: 'Литература',
+		url: 'lit',
+	},
+	{
+		title: 'История',
+		url: 'hist',
+	},
+]
+
+export default getSubjectList
