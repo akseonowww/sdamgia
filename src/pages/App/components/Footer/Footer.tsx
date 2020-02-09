bafcca45973ba5b4fa3d03ce63ad0b2d87280daa
Ilya Sidorchik <ilya@sidorchik.ru>
2020-02-10 02:39:45 +0300

    Stylelint and prettier some App components

--- src/pages/App/components/Footer/Footer.tsx
+++ src/pages/App/components/Footer/Footer.tsx
@@ -6,34 +6,34 @@ import './Footer.scss'
 import spbsut from './assets/spbsut.png'
 
 interface IFooterProps {
-	className?: string
+  className?: string
 }
 
 const Footer: FC<IFooterProps> = ({ className }) => (
-	<footer className={cx('Footer', className)}>
-		<nav className="Footer-Menu">
-			<a className="Link Link_black" href="/about">
-				О проекте
-			</a>{' '}
-			·{' '}
-			<a className="Link Link_black" href="/staff">
-				Редакция
-			</a>{' '}
-			·{' '}
-			<a className="Link Link_black" href="/legal">
-				Правовая&nbsp;информация
-			</a>{' '}
-		</nav>
-		<a
-			className="Footer-Ad"
-			href="http://priem.sut.ru/"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			<img src={spbsut} alt="СПб ГУТ!" title="priem.sut.ru" width="160" />
-		</a>
-		<div className="Footer-Copyright">©&nbsp;Гущин Д. Д., 2011—2019</div>
-	</footer>
+  <footer className={cx('Footer', className)}>
+    <nav className="Footer-Menu">
+      <a className="Link Link_black" href="/about">
+        О проекте
+      </a>{' '}
+      ·{' '}
+      <a className="Link Link_black" href="/staff">
+        Редакция
+      </a>{' '}
+      ·{' '}
+      <a className="Link Link_black" href="/legal">
+        Правовая&nbsp;информация
+      </a>{' '}
+    </nav>
+    <a
+      className="Footer-Ad"
+      href="http://priem.sut.ru/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={spbsut} alt="СПб ГУТ!" title="priem.sut.ru" width="160" />
+    </a>
+    <div className="Footer-Copyright">©&nbsp;Гущин Д. Д., 2011—2019</div>
+  </footer>
 )
 
 export default Footer
