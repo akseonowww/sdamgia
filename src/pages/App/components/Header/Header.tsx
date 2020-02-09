1217657a29045265c99c301334cc282f6b34e959
Ilya Sidorchik <ilya@sidorchik.ru>
2020-02-10 01:52:13 +0300

    Add stylelint and pre-commit hooks

--- src/pages/App/components/Header/Header.tsx
+++ src/pages/App/components/Header/Header.tsx
@@ -7,50 +7,50 @@ import 'components/Link/_selected/Link_selected.scss'
 import './Header.scss'
 
 interface IHeaderProps {
-	className?: string
+  className?: string
 }
 
 const Header: FC<IHeaderProps> = ({ className }) => (
-	<header className={cx('Header', className)}>
-		<div className="Header-Main">
-			<NavLink activeClassName="Link_selected" to="/" exact>
-				<img
-					className="Header-Logo"
-					src="https://ege.sdamgia.ru/img/headers/logo.svg"
-					alt="Сдам ГИА"
-				/>
-			</NavLink>
+  <header className={cx('Header', className)}>
+    <div className="Header-Main">
+      <NavLink activeClassName="Link_selected" to="/" exact>
+        <img
+          className="Header-Logo"
+          src="https://ege.sdamgia.ru/img/headers/logo.svg"
+          alt="Сдам ГИА"
+        />
+      </NavLink>
 
-			<div className="Header-Title">
-				<a
-					className="Header-Link Link Link_black"
-					href="https://sdamgia.ru"
-					rel="noopener noreferrer"
-				>
-					СДАМ ГИА
-				</a>
-				:{' '}
-				<NavLink
-					className="Header-Link Link Link_black"
-					activeClassName="Link_selected"
-					to="/"
-					exact
-				>
-					РЕШУ ЕГЭ
-				</NavLink>
-			</div>
-			<div className="Header-WebsiteDesc">
-				Образовательный портал для подготовки к экзаменам
-			</div>
-			<div className="Header-SubjectTitle">Математика профильного уровня</div>
-		</div>
+      <div className="Header-Title">
+        <a
+          className="Header-Link Link Link_black"
+          href="https://sdamgia.ru"
+          rel="noopener noreferrer"
+        >
+          СДАМ ГИА
+        </a>
+        :{' '}
+        <NavLink
+          className="Header-Link Link Link_black"
+          activeClassName="Link_selected"
+          to="/"
+          exact
+        >
+          РЕШУ ЕГЭ
+        </NavLink>
+      </div>
+      <div className="Header-WebsiteDesc">
+        Образовательный портал для подготовки к экзаменам
+      </div>
+      <div className="Header-SubjectTitle">Математика профильного уровня</div>
+    </div>
 
-		<img
-			className="Header-SubjectImg"
-			src="https://math.reshuct.by/img/headers/math.png"
-			alt="Математика"
-		/>
-	</header>
+    <img
+      className="Header-SubjectImg"
+      src="https://math.reshuct.by/img/headers/math.png"
+      alt="Математика"
+    />
+  </header>
 )
 
 export default Header
