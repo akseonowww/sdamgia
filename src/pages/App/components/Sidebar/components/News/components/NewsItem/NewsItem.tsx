e91e4b3a299ac203a3ee693ca61dfb1e652d43cb
Ilya Sidorchik <ilya@sidorchik.ru>
2020-02-10 23:49:06 +0300

    Typescript NewsItem

---
+++ src/pages/App/components/Sidebar/components/News/components/NewsItem/NewsItem.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react'
+import cx from 'classnames'
+
+interface INewsItem {
+  type: string
+  date?: string
+  text: string
+  url: string
+}
+
+const NewsItem: FC<INewsItem> = ({ type, date, text, url }) => {
+  return (
+    <a
+      className={cx(
+        'News-Item',
+        {
+          [`News-Item_${type}`]: type !== 'default',
+        },
+        'Link',
+        {
+          Link_static: type !== 'urgent',
+          'Link_white Link_wrap': type === 'urgent',
+        }
+      )}
+      href={url}
+      rel="noopener noreferrer"
+    >
+      {type !== 'urgent' && (
+        <>
+          <div className="News-Date Smallcapitals">{date}</div>
+          <div className="News-Text">{text}</div>
+        </>
+      )}
+
+      {type === 'urgent' && (
+        <u className="Link-U Link_white-U Link_wrap-U">{text}</u>
+      )}
+    </a>
+  )
+}
+
+export default NewsItem
