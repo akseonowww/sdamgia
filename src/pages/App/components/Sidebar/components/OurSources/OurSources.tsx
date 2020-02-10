a7748e0c822125564fa2d0d7b83d555e87f1652b
Ilya Sidorchik <ilya@sidorchik.ru>
2020-02-10 16:05:38 +0300

    Typescript OurSources

---
+++ src/pages/App/components/Sidebar/components/OurSources/OurSources.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react'
+import cx from 'classnames'
+
+import ButtonVK from 'components/ButtonVK/ButtonVK'
+import './OurSources.scss'
+
+interface IOurSources {
+  className?: string
+}
+
+const OurSources: FC<IOurSources> = ({ className }) => (
+  <div className="OurSources">
+    <div className={cx('OurSources-Desc', className)}>
+      <ButtonVK className="OurSources-ButtonVK" url="https://vk.com/reshuege">
+        Наша группа
+      </ButtonVK>
+      Мобильные приложения:
+    </div>
+    <a
+      className="OurSources-ButtonStore OurSources-ButtonStore_android"
+      href="https://play.google.com/store/apps/details?id=com.reshuege"
+      target="_blank"
+      rel="noopener noreferrer"
+      title="Андроиды"
+    >
+      {' '}
+    </a>
+    <a
+      className="OurSources-ButtonStore OurSources-ButtonStore_iOS"
+      href="https://appsto.re/ru/TSavbb.i"
+      target="_blank"
+      rel="noopener noreferrer"
+      title="Джобсы"
+    >
+      {' '}
+    </a>
+  </div>
+)
+
+export default OurSources
