0062d52fdad89834447d32b7efd7dc9db5f9035e
Ilya Sidorchik <ilya@sidorchik.ru>
2020-02-10 03:04:48 +0300

    Stylelint and prettier NavMain

--- src/pages/App/components/NavMain/NavMain.tsx
+++ src/pages/App/components/NavMain/NavMain.tsx
@@ -5,100 +5,100 @@ import { getSubjectList, ISubject, ISubjectLevel } from 'utils/subjectList'
 import './NavMain.scss'
 
 interface INavMainProps {
-	className?: string
+  className?: string
 }
 
 const NavMain: FC<INavMainProps> = ({ className }) => {
-	const [mobileMenu, setMobileMenu] = useState<boolean>(false)
-	const [subjectList, setSubjectList] = useState<Array<ISubject>>([])
+  const [mobileMenu, setMobileMenu] = useState<boolean>(false)
+  const [subjectList, setSubjectList] = useState<Array<ISubject>>([])
 
-	const exam = 'ege'
+  const exam = 'ege'
 
-	const getUrl = useCallback((popupItem, exam) => {
-		return `https://${popupItem}-${exam}.sdamgia.ru`
-	}, [])
+  const getUrl = useCallback((popupItem, exam) => {
+    return `https://${popupItem}-${exam}.sdamgia.ru`
+  }, [])
 
-	const toggleMobileMenu = useCallback(() => {
-		setMobileMenu(!mobileMenu)
-	}, [mobileMenu])
+  const toggleMobileMenu = useCallback(() => {
+    setMobileMenu(!mobileMenu)
+  }, [mobileMenu])
 
-	useEffect(() => {
-		const data = getSubjectList()
-		setSubjectList(data)
-	}, [])
+  useEffect(() => {
+    const data = getSubjectList()
+    setSubjectList(data)
+  }, [])
 
-	return (
-		<>
-			<nav
-				className={cx('NavMain', className, `${className}_mobile`)}
-				onClick={toggleMobileMenu}
-			>
-				≡ {subjectList[0] && subjectList[0].title}
-			</nav>
+  return (
+    <>
+      <nav
+        className={cx('NavMain', className, `${className}_mobile`)}
+        onClick={toggleMobileMenu}
+      >
+        ≡ {subjectList[0] && subjectList[0].title}
+      </nav>
 
-			<nav
-				className={cx(
-					'NavMain',
-					'NavMain_desktop',
-					className,
-					`${className}_desktop`,
-					{ NavMain_show: mobileMenu }
-				)}
-			>
-				{subjectList.length > 0 &&
-					subjectList.map(({ title, url, levels }, i) => {
-						if (levels) {
-							return (
-								<div
-									className="NavMain-Link NavMain_desktop-Link NavMain-PopupBlock"
-									key={i}
-								>
-									<div className="NavMain-Tab NavMain_desktop-Tab NavMain-PopupTitle">
-										≡ {title}
-									</div>
+      <nav
+        className={cx(
+          'NavMain',
+          'NavMain_desktop',
+          className,
+          `${className}_desktop`,
+          { NavMain_show: mobileMenu }
+        )}
+      >
+        {subjectList.length > 0 &&
+          subjectList.map(({ title, url, levels }, i) => {
+            if (levels) {
+              return (
+                <div
+                  className="NavMain-Link NavMain_desktop-Link NavMain-PopupBlock"
+                  key={i}
+                >
+                  <div className="NavMain-Tab NavMain_desktop-Tab NavMain-PopupTitle">
+                    ≡ {title}
+                  </div>
 
-									<div style={{ clear: 'right' }} />
-									<div className="NavMain-Popup">
-										{levels.map(
-											(levelsItem: ISubjectLevel, levelsI: number) => (
-												<a
-													className="NavMain-Link NavMain_desktop-Link"
-													href={getUrl(levelsItem.url, exam)}
-													rel="nofollow"
-													key={levelsI}
-												>
-													<div className="NavMain-Tab NavMain_desktop-Tab NavMain-PopupTab">
-														<span className="NavMain-PopupLink NavMain-PopupLink_mobile">
-															{title + ' ' + levelsItem.mobileTitle}
-														</span>
-														<span className="NavMain-PopupLink NavMain-PopupLink_desktop">
-															{levelsItem.title}
-														</span>
-													</div>
-												</a>
-											)
-										)}
-									</div>
-								</div>
-							)
-						} else {
-							return (
-								<a
-									className="NavMain-Link NavMain_desktop-Link"
-									href={getUrl(url, exam)}
-									rel="nofollow"
-									key={i}
-								>
-									<div className="NavMain-Tab NavMain_desktop-Tab">{title}</div>
-								</a>
-							)
-						}
-					})}
+                  <div className="NavMain-Ruler_clear_right" />
+                  <div className="NavMain-Popup">
+                    {levels.map(
+                      (levelsItem: ISubjectLevel, levelsI: number) => (
+                        <a
+                          className="NavMain-Link NavMain_desktop-Link"
+                          href={getUrl(levelsItem.url, exam)}
+                          rel="nofollow"
+                          key={levelsI}
+                        >
+                          <div className="NavMain-Tab NavMain_desktop-Tab NavMain-PopupTab">
+                            <span className="NavMain-PopupLink NavMain-PopupLink_mobile">
+                              {title + ' ' + levelsItem.mobileTitle}
+                            </span>
+                            <span className="NavMain-PopupLink NavMain-PopupLink_desktop">
+                              {levelsItem.title}
+                            </span>
+                          </div>
+                        </a>
+                      )
+                    )}
+                  </div>
+                </div>
+              )
+            } else {
+              return (
+                <a
+                  className="NavMain-Link NavMain_desktop-Link"
+                  href={getUrl(url, exam)}
+                  rel="nofollow"
+                  key={i}
+                >
+                  <div className="NavMain-Tab NavMain_desktop-Tab">{title}</div>
+                </a>
+              )
+            }
+          })}
 
-				<div style={{ clear: 'both' }}></div>
-			</nav>
-		</>
-	)
+        <div className="NavMain-Ruler_clear_both"></div>
+      </nav>
+    </>
+  )
 }
 
 export default NavMain
