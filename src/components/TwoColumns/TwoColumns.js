b2bb0e8e975099f1c2c49d3b225cab5023628ecc
Ilya Sidorchik <ilya@sidorchik.ru>
2020-02-13 11:23:10 +0300

    Refactor TwoColumns

--- src/components/TwoColumns/TwoColumns.js
+++ src/components/TwoColumns/TwoColumns.js
@@ -1,24 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-import Section from '../Section';
-import Grid from '../Grid';
-import './TwoColumns.scss';
+import Section from '../Section'
+import Grid from '../Grid'
+import './TwoColumns.scss'
 
-const TwoColumns = props => {
-	const { children } = props;
+const TwoColumns = ({ children }) => (
+  <Section>
+    <Grid className="TwoColumns-Grid">
+      <div className="TwoColumns-Col TwoColumns-Col_1">{children[0]}</div>
+      <div className="TwoColumns-Col TwoColumns-Col_2">{children[1]}</div>
+    </Grid>
+  </Section>
+)
 
-	return (
-		<Section>
-			<Grid className="TwoColumns-Grid">
-				<div className="TwoColumns-Col TwoColumns-Col_1">
-					{children[0]}
-				</div>
-				<div className="TwoColumns-Col TwoColumns-Col_2">
-					{children[1]}
-				</div>
-			</Grid>
-		</Section>
-	);
-};
-
-export default TwoColumns;
+export default TwoColumns
