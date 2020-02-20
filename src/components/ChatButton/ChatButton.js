fa25f4415579e4dc7d97a64d453c41eef5ad8222
Akseonov-ru <54501083+Akseonov-ru@users.noreply.github.com>
2020-02-20 17:51:52 +0300

    Typescript ChatButton (#6)

--- src/components/ChatButton/ChatButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './ChatButton.scss';
-
-const ChatButton = () => {
-	return <div className="ChatButton">Чат</div>;
-};
-
-export default ChatButton;
---
+++ src/components/ChatButton/ChatButton.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+import './ChatButton.scss'
+
+interface IChatButtonProps {}
+
+const ChatButton: FC<IChatButtonProps> = () => {
+  return <div className="ChatButton">Чат</div>
+}
+
+export default ChatButton
