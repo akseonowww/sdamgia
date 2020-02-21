f4e52dd9b86328adfee7ea2d3321cda05f2a6c51
Ilya Sidorchik <ilya@sidorchik.ru>
2020-02-21 16:46:14 +0300

    Typescript LarinVariants

--- src/components/VariantsLink/VariantsLink.tsx
+++ src/components/VariantsLink/VariantsLink.tsx
@@ -8,16 +8,21 @@ import './VariantsLink.scss'
 interface IVariantsLinkProps {
   className?: string
   to: string
+  title?: string
   children: ReactNode
   'data-wordClassName'?: string
 }
 
 const VariantsLink: FC<IVariantsLinkProps> = props => {
-  const { className, to, children } = props
+  const { className, to, children, title } = props
   const dataWordClassName = props['data-wordClassName']
 
   return (
-    <Link className={cx('Link', 'VariantsLink', className)} to={to}>
+    <Link
+      className={cx('Link', 'VariantsLink', className)}
+      to={to}
+      title={title}
+    >
       {!Number.isNaN(Number(children)) && dataWordClassName && (
         <span className={dataWordClassName}>Вариант&nbsp;</span>
       )}
