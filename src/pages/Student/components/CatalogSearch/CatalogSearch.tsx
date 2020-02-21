c9cbc5ff160bd0a1c3e965d1c510be99bec8af85
Ilya Sidorchik <ilya@sidorchik.ru>
2020-02-21 19:56:53 +0300

    Typescript CatalogSearch

---
+++ src/pages/Student/components/CatalogSearch/CatalogSearch.tsx
@@ -0,0 +1,53 @@
+import React, { FC, useState, useLayoutEffect } from 'react'
+
+import Title from 'components/Title'
+import 'components/Form/Form.scss'
+import 'components/Input/Input.scss'
+import 'components/Button/Button.scss'
+
+const useWindowWidth = (): number => {
+  const [width, setWidth] = useState(0)
+
+  useLayoutEffect(() => {
+    const updateWidth = () => setWidth(window.innerWidth)
+
+    window.addEventListener('resize', updateWidth)
+    updateWidth()
+
+    return () => window.removeEventListener('resize', updateWidth)
+  }, [])
+
+  return width
+}
+
+const CatalogSearch: FC = () => {
+  const windowWidth = useWindowWidth()
+
+  const placeholder =
+    windowWidth > 836 ? 'Номер или текст задания' : '№/текст задания'
+
+  return (
+    <div className="CatalogSearch">
+      <Title>Поиск в каталоге</Title>
+
+      <p className="Section-Desc">
+        Все задания открытого банка ЕГЭ по математике профильного уровня
+        с образцами&nbsp;решений.
+      </p>
+
+      <form action="/test" method="POST">
+        <div className="Form_oneLine">
+          <input
+            className="Input Form_oneLine-Input"
+            type="text"
+            name="id"
+            placeholder={placeholder}
+          />
+          <input className="Button" type="submit" value="Открыть" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default CatalogSearch
