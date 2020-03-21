f15a47f7aade2bfc3e36e60b04e71f6d22862abb
Ilya Sidorchik <ilya@sidorchik.ru>
2020-03-21 20:21:05 +0300

    Typescript Student

--- src/components/TwoColumns/TwoColumns.tsx
+++ src/components/TwoColumns/TwoColumns.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
 import Section from '../Section'
 import Grid from '../Grid'
 import './TwoColumns.scss'
 
-const TwoColumns = ({ children }) => (
+interface ITwoColumnsProps {
+  children: ReactNode[]
+}
+
+const TwoColumns = ({ children }: ITwoColumnsProps) => (
   <Section>
     <Grid className="TwoColumns-Grid">
       <div className="TwoColumns-Col TwoColumns-Col_1">{children[0]}</div>
