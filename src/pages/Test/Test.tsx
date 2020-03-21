2f1ed9bf899f9bb9c3f5aa1a11340a83095474e0
Ilya Sidorchik <ilya@sidorchik.ru>
2020-03-21 20:11:42 +0300

    Typescript Test

---
+++ src/pages/Test/Test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Section from 'components/Section'
+import Title from 'components/Title'
+
+const Test = () => (
+  <Section className="Test">
+    <Title>Тест</Title>
+    <p className="Section-Desc">
+      При выполнении заданий с кратким ответом впишите в поле для ответа цифру,
+      которая соответствует номеру правильного ответа, или число, слово,
+      последовательность букв (слов) или цифр. Ответ следует записывать без
+      пробелов и каких-либо дополнительных символов. Дробную часть отделяйте
+      от целой десятичной запятой. Единицы измерений писать не нужно.
+    </p>
+  </Section>
+)
+
+export default Test
