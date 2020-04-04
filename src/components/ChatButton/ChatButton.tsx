3996e8ed9725ab66933aa457cccdd082b18d8c69
Axeonov Ruslan <akseonov.ru@ya.ru>
2020-04-04 19:35:53 +0300

    BEM ChatButton, Section, Grid and TwoColumns with bem-react

--- src/components/ChatButton/ChatButton.tsx
+++ src/components/ChatButton/ChatButton.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react'
+import { cn } from '@bem-react/classname'
+import { classnames as cx } from '@bem-react/classnames'
 
 import './ChatButton.scss'
 
 interface IChatButtonProps {}
 
-const ChatButton: FC<IChatButtonProps> = () => {
-  return <div className="ChatButton">Чат</div>
+const ChatButton = ({}: FC<IChatButtonProps>) => {
+  const chatButton = cn('ChatButton')
+
+  return <div className={cx(chatButton())}>Чат</div>
 }
 
 export default ChatButton
