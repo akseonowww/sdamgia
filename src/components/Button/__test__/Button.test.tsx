5c307cce64ee4fa904cb28e60a217d00fb4fa16a
Ilya Sidorchik <ilya@sidorchik.ru>
2020-04-10 18:15:43 +0300

    Test Button

---
+++ src/components/Button/__test__/Button.test.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { unmountComponentAtNode, render } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import pretty from 'pretty'
+
+import Button from '../Button'
+
+describe('Button', () => {
+  const handleClick = jest.fn()
+  let container: HTMLElement | null = null
+
+  beforeEach(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+  })
+
+  afterEach(() => {
+    if (container) {
+      unmountComponentAtNode(container)
+      container.remove()
+      container = null
+    }
+  })
+
+  it('renders with required and optional props', () => {
+    act(() => {
+      render(<Button>Open</Button>, container)
+    })
+    expect(container && container.textContent).toBe('Open')
+
+    act(() => {
+      render(
+        <Button
+          className="ConstructorForm-SubmitButton"
+          type="submit"
+          tabIndex={0}
+          handleClick={handleClick}
+        >
+          Make work from 5 tasks
+        </Button>,
+        container
+      )
+    })
+    expect(container && container.textContent).toBe('Make work from 5 tasks')
+  })
+
+  it('matches snapshot', () => {
+    act(() => {
+      render(<Button>Open</Button>, container)
+    })
+    if (container) expect(pretty(container.innerHTML)).toMatchSnapshot()
+
+    act(() => {
+      render(
+        <Button
+          className="ConstructorForm-SubmitButton"
+          type="submit"
+          tabIndex={0}
+          handleClick={handleClick}
+        >
+          Make work from 5 tasks
+        </Button>,
+        container
+      )
+    })
+    if (container) expect(pretty(container.innerHTML)).toMatchSnapshot()
+  })
+})
