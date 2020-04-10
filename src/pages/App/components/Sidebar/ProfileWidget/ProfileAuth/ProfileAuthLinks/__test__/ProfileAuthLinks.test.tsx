446b25306e58be4e52735a457b9e2f05d954c347
Ilya Sidorchik <ilya@sidorchik.ru>
2020-04-10 19:10:44 +0300

    Test ProfileAuthLinks

---
+++ src/pages/App/components/Sidebar/ProfileWidget/ProfileAuth/ProfileAuthLinks/__test__/ProfileAuthLinks.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { unmountComponentAtNode, render } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import pretty from 'pretty'
+import { cn } from '@bem-react/classname'
+
+import ProfileAuthLinks from '../ProfileAuthLinks'
+
+describe('ProfileAuthLinks', () => {
+  const handleClick = jest.fn()
+  const profileAuth = cn('ProfileAuth')
+  let container: HTMLElement | null = null
+
+  beforeEach(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+  })
+
+  afterEach(() => {
+    if (container) {
+      unmountComponentAtNode(container)
+      container.remove()
+      container = null
+    }
+  })
+
+  it('renders with required and optional props', () => {
+    act(() => {
+      render(<ProfileAuthLinks blockClassName={profileAuth} />, container)
+    })
+    expect(container && container.textContent).toBe(
+      'ЗарегистрироватьсяВосстановление пароляВход через Вконтакте'
+    )
+  })
+
+  it('matches snapshot', () => {
+    act(() => {
+      render(<ProfileAuthLinks blockClassName={profileAuth} />, container)
+    })
+    if (container) expect(pretty(container.innerHTML)).toMatchSnapshot()
+  })
+})
