86463627216e9e36a99152a2f9f533941a8222cd
Ruslan Aksenov <your_email@whatever.com>
2020-04-19 10:30:50 +0300

    BEM in App with @bem-react

--- src/pages/App/components/HelpButton/HelpButton.tsx
+++ src/pages/App/components/HelpButton/HelpButton.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { cn } from '@bem-react/classname'
+import { classnames as cx } from '@bem-react/classnames'
 
 import './HelpButton.scss'
 
-const HelpButton = () => <div className="HelpButton"></div>
+const HelpButton = () => {
+  const helpButton = cn('HelpButton')
+
+  return
+  ;<div className={cx(helpButton())}></div>
+}
 
 export default HelpButton
